362344905d5444a7516f60fdc76af33dcbb97553
Abrar Musa <abrar.musa.89@gmail.com>
2025-02-10 15:00:45 -0500

    t

--- app/ai/chat.ts
+++ app/ai/chat.ts
@@ -22,6 +22,7 @@ const startCompletion: (props: StartCompletionProps) => Promise<any> = async ({ 
     const obj: any = {
         // model: "meta-llama/Llama-3.3-70B-Instruct", // Changed from gpt-4o to gpt-4
         model: "gpt-4o-mini",
+        
         messages: messages,
         tool_choice: "auto", 
         tools: tools.map((tool: any) => ({ ...tool, type: "function" })),
